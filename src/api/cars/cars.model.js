8110bcb64b526a647acd20543bd129a2f20da80f
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-05 12:22:44 +0200

    cambios en modelo car

--- src/api/cars/cars.model.js
+++ src/api/cars/cars.model.js
@@ -1,27 +1,66 @@
 const mongoose = require("mongoose");
-const CarsSchema = new mongoose.Schema({
-  brand: [{ type: mongoose.Types.ObjectId, ref: 'brands'}],
-  model: { type: String, required: true, unique: true },
-  type: { 
-    type: String, 
-    required: true,
-    enum:['Sedán', 'Compacto', 'Cuopé', 'Roadster', 'Ranchera'] },
-  power: { type: Number, required: true },
-  par: { type: Number, required: true },
-  engineType: { type: String, required: true },
-  engineCil: { type: Number, required: true },
-  traction: {
-    type: String,
-    required: true,
-    enum:['Trasera', 'Delantera', '4X4'] },
-  year: { type: Number, required: true },
-  heigh: { type: Number, required: true },
-  speed: { type: Number, required: true },
-  image: { type: String, required: true },
-},{
-  timestamps: true,
-  collection: 'cars'
-});
+const CarsSchema = new mongoose.Schema(
+  {
+    brand: {
+      type: String,
+      enum: [
+        "Seat",
+        "Renault",
+        "Peugeot",
+        "Citroën",
+        "Ford",
+        "Opel",
+        "Volkswagen",
+        "Toyota",
+        "Nissan",
+        "Fiat",
+        "Kia",
+        "Hyundai",
+        "Bmw",
+        "Mercedes-Benz",
+        "Audi",
+        "Volvo",
+        "Mazda",
+        "Skoda",
+        "Mini",
+        "Land Rover",
+        "Jeep",
+        "Mitsubishi",
+        "Suzuki",
+        "Honda",
+        "Alfa Romeo",
+        "Lexus",
+        "Subaru",
+        "Porsche",
+        "Jaguar",
+        "Smart",
+      ],
+    },
+    model: { type: String, required: true, unique: true },
+    type: {
+      type: String,
+      required: true,
+      enum: ["Sedán", "Compacto", "Cuopé", "Roadster", "Ranchera"],
+    },
+    power: { type: Number, required: true },
+    par: { type: Number, required: true },
+    engineType: { type: String, required: true },
+    engineCil: { type: Number, required: true },
+    traction: {
+      type: String,
+      required: true,
+      enum: ["Trasera", "Delantera", "4X4"],
+    },
+    year: { type: Number, required: true },
+    heigh: { type: Number, required: true },
+    speed: { type: Number, required: true },
+    image: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    collection: "cars",
+  }
+);
 
-const Car = mongoose.model('cars', CarsSchema);
-module.exports =  Car;
+const Car = mongoose.model("cars", CarsSchema);
+module.exports = Car;
