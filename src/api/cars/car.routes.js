48c2b43068e40cccb88c595b8becab46c1aa55eb
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-26 10:26:36 +0200

    rutas

--- src/api/cars/car.routes.js
+++ src/api/cars/car.routes.js
@@ -3,10 +3,10 @@ const uploadImage = require("../middlewares/file");
 const { isAuth, isAdmin } = require("../middlewares/auth");
 const carRoutes = require('express').Router();
 
-carRoutes.post("/create", uploadImage.single('image'),  createCar)
+carRoutes.post("/create", [isAdmin], uploadImage.single('image'),  createCar)
 carRoutes.put("/update/:id", [isAdmin],uploadImage.single('image'), updateCarById)
-carRoutes.delete("/delete/:id", deleteCarById)
-carRoutes.get("/:id", getCarByid)
-carRoutes.get("/",  getCars)
+carRoutes.delete("/delete/:id", [isAdmin], deleteCarById)
+carRoutes.get("/:id", [isAdmin],[isAuth],getCarByid)
+carRoutes.get("/", [isAuth],  getCars)
 
 module.exports = { carRoutes }
