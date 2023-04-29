36ac78724349e27c3eddf793092a5315bc92a0ed
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-29 09:01:52 +0200

    productos

--- src/api/carsToRevise/cars-to-revise.model.js
+++ src/api/carsToRevise/cars-to-revise.model.js
@@ -37,7 +37,7 @@ const CarsSchema = new mongoose.Schema({
     required: true
   },
   model: { type: String, required: true, unique: true },
-  type: { 
+/*   type: { 
     type: String, 
     required: true,
     enum:['Sedán', 'Compacto', 'Deportivo', 'Roadster', 'Ranchera'] },
@@ -51,7 +51,7 @@ const CarsSchema = new mongoose.Schema({
     enum:['Trasera', 'Delantera', '4X4'] },
   year: { type: Number, required: true },
   heigh: { type: Number, required: true },
-  speed: { type: Number, required: true },
+  speed: { type: Number, required: true }, */
   image: { type: String, required: true },
 },{
   timestamps: true,
