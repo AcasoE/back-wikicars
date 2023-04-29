a187284419b1b3491dbf14e38eb8bdbd9e0204e6
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-29 13:26:53 +0200

    productos

--- src/api/users/users.controllers.js
+++ src/api/users/users.controllers.js
@@ -73,20 +73,7 @@ const isAdminGet = async (req, res, next) => {
 
 const checkSession = async (re, res, next ) =>{
     try {
-        const token = req.headers.authorization
-        if (!token) {
-            return res.json('No estás autorizado')
-        }
-        const validToken = verifyToken(token)
-        if(validToken){
-            const userloged = await User.findById(validToken.id)
-            userloged.password = null
-            req.user = userloged
-            return res.status(200).json(userloged)
-            
-        } else{
-            return res.json("No eres admin")
-        }
+        return req.res
     
     } catch (error) {
         return next(error)
