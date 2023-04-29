adb0cb4385f8692d4431c45faa3cefbe9b7c2502
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-29 13:05:01 +0200

    productos

--- src/api/users/users.controllers.js
+++ src/api/users/users.controllers.js
@@ -71,9 +71,31 @@ const isAdminGet = async (req, res, next) => {
     }
 }
 
+const getSession = async (re, res, next ) =>{
+    try {
+        const token = req.headers.authorization
+        if (!token) {
+            return res.json('No estás autorizado')
+        }
+        const validToken = verifyToken(token)
+        if(validToken){
+            const userloged = await User.findById(validToken.id)
+            userloged.password = null
+            req.user = userloged
+            return res.status(200).json(userloged)
+            
+        } else{
+            return res.json("No eres admin")
+        }
+    
+    } catch (error) {
+        return next(error)
+    }
+}
 
 
 
 
 
-module.exports = { getAllUsers, registerUser, loginUser, isAdminGet}
+
+module.exports = { getAllUsers, registerUser, loginUser, isAdminGet, getSession}
--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -1,9 +1,10 @@
 const { isAdmin, isAuth } = require('../middlewares/auth')
-const { getAllUsers, registerUser, loginUser, isAdminGet } = require('./users.controllers')
+const { getAllUsers, registerUser, loginUser, isAdminGet, getSession } = require('./users.controllers')
 const userRoutes = require('express').Router()
 userRoutes.get("/rol",[isAuth], isAdminGet)
 userRoutes.post("/register", registerUser)
 userRoutes.post("/login", loginUser)
+userRoutes.get("/",[isAuth], getSession )
 userRoutes.get("/",[isAdmin] ,getAllUsers)
 
 module.exports = {userRoutes}
