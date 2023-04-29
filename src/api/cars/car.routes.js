bdb4c0e1be145a69414af5cfa10e415c43f75563
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-04-29 11:00:27 +0200

    productos

--- src/api/cars/car.routes.js
+++ src/api/cars/car.routes.js
@@ -3,7 +3,7 @@ const uploadImage = require("../middlewares/file");
 const { isAuth, isAdmin } = require("../middlewares/auth");
 const carRoutes = require('express').Router();
 
-carRoutes.post("/create",  uploadImage.single('image'), createCar)
+carRoutes.post("/create",[isAuth],  uploadImage.single('image'), createCar)
 carRoutes.put("/update/:id", [isAdmin],uploadImage.single('image'), updateCarById)
 carRoutes.delete("/delete/:id", [isAdmin], deleteCarById)
 carRoutes.get("/:id",getCarByid)
